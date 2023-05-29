453530d6534e760f2188dc444aa23367451b0095
emtinanseo <emtinan.s.e.osman.com>
2023-05-29 14:40:21 +0300

    use Playwright in headless mode

--- dockerized/kamernet_scraper/scrapper_scripts/kamernet_get_daily.py
+++ dockerized/kamernet_scraper/scrapper_scripts/kamernet_get_daily.py
@@ -79,7 +79,7 @@ async def main():
     dailyLinks = []
 
     async with async_playwright() as player:
-        browser = await player.chromium.launch(headless=False)
+        browser = await player.chromium.launch(headless=True)
         ua = ("Mozilla/5.0 (X11; Linux x86_64)"
             "AppleWebKit/537.36 (KHTML, like Gecko)"
             "Chrome/113.0.0.0 Safari/537.36")
