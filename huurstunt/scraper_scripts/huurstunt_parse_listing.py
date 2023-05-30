1671cc28e9b493d408ea4e981f8725674af9687e
Angeleah <a.hoeppner@yahoo.ca>
2023-05-30 15:04:07 -0700

    Testing code removed

--- huurstunt/scraper_scripts/huurstunt_parse_listing.py
+++ huurstunt/scraper_scripts/huurstunt_parse_listing.py
@@ -4,82 +4,12 @@ import asyncio
 from playwright_stealth import stealth_async
 import datetime
 import sys
-import time
 
-# URL = "https://www.funda.nl/huur/bleiswijk/huis-88443766-van-kinsbergenstraat-11/" #Dutch URL for testing
-# URL = "https://www.funda.nl/en/huur/amsterdam/huis-42085123-cannenburg-15/" #English URL for testing
 
 # *** Methods *** #
 
 
-def readFile(inputFile: str) -> list:
-    """Tries to open the passed in file
-
-    :param {string} inputFile - The name of the file to be opened
-
-    :return A list containing the urls of listings contained in the inputFile
-
-    :throws exception if unable to find the file to open
-    """
-    try:
-        openedFile = open(inputFile, "r")
-        listingURLs = openedFile.read()
-        openedFile.close()
-        return listingURLs
-    except Exception as err:
-        print(err)
-
-
-async def stillAvailable(page) -> bool:
-    """Checks to see if the property displayed on the page is still available for rent or sale
-
-    :param {page object} page - The browser page displaying a listing
-
-    :return True - If definitief is not a classname on the page, 
-    False - If definitief is a classname on the page
-    """
-    # Gets the object that contains the status of the listing: new, not available
-    selElement = await page.wait_for_selector(
-        ".object-header__labels ul li")
-    # Gets the class list of the element found
-    selClass = await selElement.get_attribute("class")
-
-    return "definitief" not in str(selClass)
-
-
-# async def getFeatures(page) -> list:
-#     """Gets the name and specifics of each detail listed on the page
-
-#     :param {page object} page - The browser page displaying a listing
-#     :return A list of small dictionaries of strings of all the details {title: name}
-#     """
-#     allFeatures = []
-
-#     # Selects all dt's that are children of the class
-#     allFeatureTitles = await page.query_selector_all(
-#         ".object-kenmerken-list dt")
-
-#     # Select all dd's that are immediately following a dt
-#     allFeatureDetails = await page.query_selector_all(
-#         ".object-kenmerken-list dt + dd")
-
-#     for i in range(len(allFeatureTitles)):
-#         title = await allFeatureTitles[i].inner_text()
-#         detail = await allFeatureDetails[i].inner_text()
-#         allFeatures.append({title: detail})
-
-#     return allFeatures
-
-async def get_noscript(txt):
-    split_str = await txt.inner_html()
-    links = set()
-    for i in str(split_str).strip().split(" "):
-        if "https://casco-media-prod.global.ssl.fastly.net" in i:
-            links.add(i[i.find('http'):])
-    return links
-
-
-async def writeJson(fileName, listingInfo):
+async def write_file(fileName, listingInfo):
     """Writes the listing info to a file with the passed in fileName
 
     :param {string} fileName - The string containing the name the file will receive
@@ -88,16 +18,18 @@ async def writeJson(fileName, listingInfo):
     with open(f"huurstunt/scraper_scripts/listings/{fileName}", "a") as outfile:
         outfile.write(json.dumps(listingInfo, indent=4))
 
+scrapeDate = str(datetime.date.today())
 
-async def writeToFile(listingInfo):
-    """Checks to see if the listing is a rental or a sale and sets the filename accordingly
+
+async def set_file_name(listingInfo):
+    """Sets the filename and passes the name and information to be written to file
 
     :param {dict} listingInfo - A dictionary containing all the information for each listing
     """
 
     fileName = f"rental--{scrapeDate}--{listingInfo['address']}.json"
 
-    await writeJson(fileName.replace(" / ", "-").strip(), listingInfo)
+    await write_file(fileName.replace(" / ", "-").strip(), listingInfo)
 
 
 async def getPhotos(page) -> list:
@@ -105,18 +37,14 @@ async def getPhotos(page) -> list:
 
     :param {page object} page - The browser page displaying a listing
 
-    :return A list containing the src to all the photos of the current listing"""
+    :return A list containing the src to all the photos of the current listing
+    """
+
     photos = set()
     images = await page.query_selector_all(".rental-image-gallery__link img")
 
-    noscript_links = await page.query_selector("noscript")
-    image_links = await get_noscript(noscript_links)
-    for image in image_links:
-        photos.add(image)
     for i in range(len(images)):
         imageSRC = await images[i].get_attribute("src")
-        # Checks to see if it is one of the listing photos
-        # if "https://casco-media-prod.global.ssl.fastly.net" in imageSRC:
         photos.add(imageSRC)
 
     return list(photos)
@@ -126,7 +54,7 @@ async def getFeatures(page) -> list:
     """Gets the name and specifics of each detail listed on the page
 
     :param {page object} page - The browser page displaying a listing
-    :return A list of small dictionaries of strings of all the details {title: name}
+    :return A list of small dictionaries of strings of all the details {title: detail}
     """
     allFeatures = []
     sections = await page.query_selector_all(".info-wrapper")
@@ -145,7 +73,6 @@ async def getFeatures(page) -> list:
                 detail = await allFeatureDetails[i].inner_text()
                 allFeatures.append(f"{section_heading} - {title}: {detail}")
 
-    print(f"Number of features: {len(allFeatures)}")
     return allFeatures
 
 
@@ -161,14 +88,14 @@ async def getDetail(elSelector, page) -> str:
 
 
 async def getInfo(cut_off_date, page) -> dict:
-    """Gets all the informatin for the listing that the page is at
+    """Gets all the informatin for the listing that the page is at prior to the cut off date
 
+    :param {date} cut_off_date - The listed date must be this day or after
     :param {page object} page - The browser page displaying a listing
 
     :return A dictionary containing the information: title, address, url, features, and photos
     """
     try:
-
         await page.wait_for_selector(".title__listing", timeout=5000)
         # Get the listed next to "Home active since" text - this is the date the home was listed
         posted_date = await page.query_selector_all(
@@ -200,28 +127,23 @@ async def run(link, page):
     """
 
     today = datetime.date.today().strftime('%d-%m-%Y')
-    print(f"Today: {today}")
     cut_off_date = (datetime.datetime.strptime(
         today, "%d-%m-%Y") - datetime.timedelta(days=1)).strftime("%d-%m-%Y")
-    print(f"Cut off: {cut_off_date}")
 
     try:
         await page.goto(link)
-        print(f"Went to {link}")
         info = await getInfo(cut_off_date, page)
         if info:
-            await writeToFile(info)
+            await set_file_name(info)
     except Exception as err:
         print(f"Error {link} {err}")
 
-scrapeDate = str(datetime.date.today())
-
-test_link = "https://www.huurstunt.nl/huurwoning/huren/in/amsterdam/paul-scholtenstraat/HK5BI"
-
 
 async def main():
-    """Reads the list of all the sales and rental links for each gemeenten"""
-    # Use scrapeDate for live - It is set to today()
+    """
+    Visits the Huurstunt all listings for the Netherlands and iterates through the listings
+    getting their details until the cut off date is passed
+    """
 
     async with async_playwright() as player:
         # User agent must be set for stealth mode so the captcha isn't triggered in headless mode.
@@ -238,20 +160,22 @@ async def main():
         for i in range(15):
             link = f"https://www.huurstunt.nl/huren/nederland/p{i+1}"
             await page.goto(link, wait_until="domcontentloaded")
-            print(f"Listings page {i+1}")
-            # search_results = page.locator('a[href*="/huren/"]')
+
+            # Waits until the listing details are fully loaded
             await page.wait_for_selector(".boxed-widget--clean > div a")
             search_results = await page.query_selector_all(
                 ".boxed-widget--clean > div a")
-            print(f"There were {len(search_results)} links found")
+
             links = []
+            # Saves each listing link found to the links list for iteration after listing page is left
             for result in search_results:
                 href = await result.get_attribute("href")
                 links.append(href)
-                # print(f"link {href}")
+            # All the links found on the current page
             for link in links:
-                if "/huren/" in href:
+                if "/huren/" in href and "contact" not in href:
                     await run(f"{main_page}{link}", page)
+
         await ctx.close()
         await browser.close()
 
