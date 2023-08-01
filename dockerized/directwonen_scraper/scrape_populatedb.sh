54f416db49ae60db8485198ac0c74ef28870704d
emtinanseo <emtinan.s.e.osman.com>
2023-08-01 18:27:50 +0300

    update the bash script to reflect name change

--- dockerized/directwonen_scraper/scrape_populatedb.sh
+++ dockerized/directwonen_scraper/scrape_populatedb.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-python3 scrapper_scripts/directwonen_get_daily.py
-echo "get_daily script has run!"
-python3 scrapper_scripts/directwonen_parse_listing.py
-echo "parse_listing script has run!"
+python3 scrapper_scripts/directwonen_get_listings.py
+echo "get_listings script has run!"
 python3 db_scripts/db.py
 echo "db was created"
 python3 db_scripts/json_to_db.py
